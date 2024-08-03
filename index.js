524bd87497ade001f1d13d1270746930e7efe16f
kou-96 <a.chachamaru.k@gmail.com>
2024-08-03 14:25:01 +0900

    TODO サーバ側

---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const cors = require("cors");
+const pool = require("./db");
+const app = express();
+const PORT = 5003;
+
+const corsOption = {
+  origin: ["http://localhost:5173"],
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOption));
+app.use(express.json());
+
+app.get("/todos", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM tasks");
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("タスクの取得に失敗しました");
+  }
+});
+
+app.post("/tasks", async (req, res) => {
+  const { description } = req.body;
+
+  if (!description) {
+    return res.status(400).json({ error: "入力は必須です" });
+  }
+
+  try {
+    const result = await pool.query(
+      "INSERT INTO tasks (description) VALUES ($1) RETURNING *",
+      [description]
+    );
+    const newTask = result.rows[0];
+    res.status(201).json(newTask);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "内部サーバーエラーが発生しました" });
+  }
+});
+
+app.patch("/tasks/:id", async (req, res) => {
+  const { id } = req.params;
+  const { completed } = req.body;
+
+  try {
+    const result = await pool.query(
+      "UPDATE tasks SET completed = $1 WHERE id = $2 RETURNING *",
+      [completed, id]
+    );
+    res.json(result.rows[0]);
+  } catch (err) {
+    res.status(500).json({ error: "Error updating todo" });
+  }
+});
+
+app.delete("/tasks/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const result = await pool.query(
+      "DELETE FROM tasks WHERE id = $1 RETURNING *",
+      [id]
+    );
+    if (result.rowCount === 0) {
+      return res.status(404).json({ error: "タスクが見つかりませんでした。" });
+    }
+    const deletedTask = result.rows[0];
+    res
+      .status(200)
+      .json({ message: "タスクは正常に削除されました。", task: deletedTask });
+  } catch (err) {
+    console.error("タスクの削除中にエラーが発生しました。", err);
+    res.status(500).json({ error: "内部サーバエラーが発生しました。" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`サーバー${PORT}を立ち上げました。`);
+});
