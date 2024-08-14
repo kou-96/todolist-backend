91e0f322acfa1c60a8e44c25707c1d7535aad00f
kou-96 <a.chachamaru.k@gmail.com>
2024-08-14 23:24:39 +0900

    cookie追加に伴ってuser_idの追加

--- index.js
+++ index.js
@@ -12,13 +12,6 @@ const corsOption = {
 app.use(cors(corsOption));
 app.use(express.json());
 
-app.get("/users", (req, res) => {
-  pool.query("SELECT * FROM users", (errors, results) => {
-    if (errors) throw errors;
-    return res.status(200).json(results.rows);
-  });
-});
-
 app.get("/todos", async (req, res) => {
   try {
     const result = await pool.query("SELECT * FROM tasks");
@@ -27,6 +20,27 @@ app.get("/todos", async (req, res) => {
     console.error(error);
     res.status(500).send("タスクの取得に失敗しました");
   }
+});
+
+app.get("/todos/:user_id", async (req, res) => {
+  const { user_id } = req.params;
+
+  try {
+    const result = await pool.query("SELECT * FROM tasks WHERE user_id = $1", [
+      user_id,
+    ]);
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("タスクの取得に失敗しました");
+  }
+});
+
+app.get("/users", (req, res) => {
+  pool.query("SELECT * FROM users", (errors, results) => {
+    if (errors) throw errors;
+    return res.status(200).json(results.rows);
+  });
 });
 
 app.post("/users/signup", async (req, res) => {
@@ -39,7 +53,7 @@ app.post("/users/signup", async (req, res) => {
   }
 
   try {
-    const result = await pool.query(
+    const allUsers = await pool.query(
       "SELECT * FROM users WHERE username = $1 OR email = $2",
       [username, email]
     );
@@ -47,7 +61,7 @@ app.post("/users/signup", async (req, res) => {
     let usernameError = false;
     let emailError = false;
 
-    for (const row of result.rows) {
+    for (const row of allUsers.rows) {
       if (row.username === username) {
         usernameError = true;
       }
@@ -72,13 +86,16 @@ app.post("/users/signup", async (req, res) => {
         .json({ message: "このメールアドレスは既に使用されています。" });
     }
 
-    await pool.query(
-      "INSERT INTO users (username, email, password) VALUES ($1, $2, $3)",
+    const result = await pool.query(
+      "INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *",
       [username, email, password]
     );
     res
       .status(201)
-      .json({ message: "ユーザーの作成に成功しました。", username });
+      .json({
+        message: "ユーザーの作成に成功しました。",
+        data: result.rows[0],
+      });
   } catch (err) {
     console.error("Error executing query:", err);
     res.status(500).json({ message: "ユーザーの作成に失敗しました。" });
@@ -107,9 +124,7 @@ app.post("/users/login", async (req, res) => {
     }
 
     const user = result.rows[0];
-    res
-      .status(200)
-      .json({ message: "ログインに成功しました。", username: user.username });
+    res.status(200).json({ message: "ログインに成功しました。", data: user });
   } catch (err) {
     console.error("Error executing query:", err);
     res.status(500).json({ message: "ログインに失敗しました。" });
@@ -117,7 +132,7 @@ app.post("/users/login", async (req, res) => {
 });
 
 app.post("/tasks", async (req, res) => {
-  const { description } = req.body;
+  const { user_id, description } = req.body;
 
   if (!description) {
     return res.status(400).json({ error: "入力は必須です" });
@@ -125,8 +140,8 @@ app.post("/tasks", async (req, res) => {
 
   try {
     const result = await pool.query(
-      "INSERT INTO tasks (description) VALUES ($1) RETURNING *",
-      [description]
+      "INSERT INTO tasks (user_id, description) VALUES ($1,$2) RETURNING *",
+      [user_id, description]
     );
     const newTask = result.rows[0];
     res.status(201).json(newTask);
